bd0cb448d956ba2c9fe354c8d4bab16c7861bb34
Arco Mul <arco.mul@denkwerk.com>
2026-03-25 16:52:12 +0100

    fix: prefer eslint over other lsp formatters when eslint is active

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,17 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      format = {
+        filter = function(client, bufnr)
+          -- If eslint is attached (meaning it found a config), prefer it
+          local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "eslint" })
+          if #clients > 0 then
+            return client.name == "eslint"
+          end
+          -- Otherwise fall through to other formatters
+          return true
+        end,
+      },
     },
   },
 }
